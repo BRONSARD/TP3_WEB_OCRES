f39ee8bb253f0bac0a5731ff6c780525ca113926
Essec <mathieu.bronsard@gmail.com>
2021-10-04 15:50:16 +0200

    Last Commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ function start() {
   // Création de l'objet apiWeather
   const apiWeather = new API_WEATHER();
   // Appel de la fonction fetchTodayForecast
-
   affichage(apiWeather);
 }
 
@@ -16,22 +15,24 @@ function actualiser(){
 
 function affichage(apiWeather){
   apiWeather
-    .fetchTodayForecast()
+    .getThreeDayForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
       // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      for(var i = 0; i < 4;i++) {
+      const main = data.list[i].weather[0].main;
+      const description = data.list[i].weather[0].description;
+      const temp = data.list[i].main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      document.getElementById(`today-forecast-main-${i}`).innerHTML = main;
+      document.getElementById(`today-forecast-more-info-${i}`).innerHTML = description;
+      document.getElementById(`icon-weather-container-${i}`).innerHTML = icon;
+      document.getElementById(`today-forecast-temp-${i}`).innerHTML = `${temp}°C`;
+      }
     })
     .catch(function(error) {
       // Affiche une erreur
